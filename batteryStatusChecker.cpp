2adc5a5c6efa5f73584a13187395f86b377f062a
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-20 12:43:26 +0530

    Add files via upload

---
+++ batteryStatusChecker.cpp
@@ -0,0 +1,30 @@
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+#define MAX_TEMPERATURE = 45.0;
+#define MIN_TEMPERATURE = 0.0;
+#define MAX_SOC = 80.0;
+#define MIN_SOC = 20.0;
+#define MAX_CHARGERATE = 0.8;
+#define MIN_CHARGERATE = 0.0;
+
+bool checkValueInRange(float parameterValue, float maxLimit, float minLimit)
+{
+  if((parameterValue < maxLimit) && (parameterValue > minLimit))
+    return true;
+  else
+    return false;
+}
+
+bool isBatteryStatusOk(float temperature, float SOC, float chargeRate)
+{
+  bool temperatureCheck = checkValueInRange(temperature,MAX_TEMPERATURE,MIN_TEMPERATURE);
+  bool SOCCheck = checkValueInRange(SOC,MAX_SOC,MIN_SOC);
+  bool chargerRateCheck = checkValueInRange(chargeRate,MAX_CHARGERATE,MIN_CHARGERATE);
+
+  if(temperatureCheck && SOCCheck && chargerRateCheck)
+    return true;
+  else
+    return false;
+}
---
+++ batteryStatusChecker.hpp
@@ -0,0 +1,6 @@
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+bool checkValueInRange(float parameterValue, float maxLimit, float minLimit);
+bool isBatteryStatusOk(float temperature, float SOC, float chargeRate);
