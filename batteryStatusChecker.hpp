2300675270f34c0a0fba5688fd614b897130b233
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-20 13:02:51 +0530

    Update batteryStatusChecker.hpp

--- batteryStatusChecker.hpp
+++ batteryStatusChecker.hpp
@@ -1,6 +1,3 @@
-#include <assert.h>
-#include <iostream>
-using namespace std;
 
 bool checkValueInRange(float parameterValue, float maxLimit, float minLimit);
 bool isBatteryStatusOk(float temperature, float SOC, float chargeRate);
