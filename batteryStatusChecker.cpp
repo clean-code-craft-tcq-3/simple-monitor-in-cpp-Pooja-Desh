c93595570dd900d7957a025ad991bd87b0e87e8e
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-20 12:52:46 +0530

    Update batteryStatusChecker.cpp

--- batteryStatusChecker.cpp
+++ batteryStatusChecker.cpp
@@ -4,12 +4,7 @@
 
 using namespace std;
 
-float MAX_TEMPERATURE = 45.0;
-float MIN_TEMPERATURE = 0.0;
-float MAX_SOC = 80.0;
-float MIN_SOC = 20.0;
-float MAX_CHARGERATE = 0.8;
-float MIN_CHARGERATE = 0.0;
+
 
 bool checkValueInRange(float parameterValue, float maxLimit, float minLimit)
 {
@@ -21,6 +16,13 @@ bool checkValueInRange(float parameterValue, float maxLimit, float minLimit)
 
 bool isBatteryStatusOk(float temperature, float SOC, float chargeRate)
 {
+  float MAX_TEMPERATURE = 45.0;
+  float MIN_TEMPERATURE = 0.0;
+  float MAX_SOC = 80.0;
+  float MIN_SOC = 20.0;
+  float MAX_CHARGERATE = 0.8;
+  float MIN_CHARGERATE = 0.0;
+  
   bool temperatureCheck = checkValueInRange(temperature,MAX_TEMPERATURE,MIN_TEMPERATURE);
   bool SOCCheck = checkValueInRange(SOC,MAX_SOC,MIN_SOC);
   bool chargerRateCheck = checkValueInRange(chargeRate,MAX_CHARGERATE,MIN_CHARGERATE);
