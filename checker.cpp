5c39bb3e8aed65b989f6b2cb00280e0e92721751
Pooja-Desh <100403024+Pooja-Desh@users.noreply.github.com>
2022-08-20 12:54:36 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -7,6 +7,7 @@ int main()
 {
   assert(isBatteryStatusOk(25, 70, 0.7) == true);
   assert(isBatteryStatusOk(50, 85, 0) == false);
-  assert(isBatteryStatusOk(-20, 50, 0.2) == false);
+  assert(isBatteryStatusOk(20, 50, 0.2) == true);
   assert(isBatteryStatusOk(45, 80, 0.8) == true);
+  return 1;
 }
